67d1c858ec7772ea6a6ba5863434e9ddae3aa015
Daniel <dtujo@scotforge.com>
2025-04-25 17:04:36 -0500

    Innit Commit

---
+++ TaskTurner/Models/Task.cs
@@ -0,0 +1,54 @@
+﻿namespace TaskTurner.Models;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime StartDate { get; set; }
+    public bool IsComplete { get; set; }
+    public TimeSpan Timer { get; set; }
+    
+    public TaskState TaskState { get; set; }
+    public TaskImportance TaskImportance { get; set; }
+    public TaskCategory TaskCategory { get; set; }
+    
+}
+
+public enum TaskState
+{
+    InProgress,
+    Complete,
+    NotStarted,
+    Late,
+    Archived,
+    Deleted
+}
+
+public enum TaskCategory
+{
+    Work,
+    Personal,
+    Home,
+    HealthWellbeing,
+    Finance,
+    Shopping,
+    SocialFamily,
+    Education,
+    Travel,
+    Errands,
+    HobbiesLeisure,
+    VolunteeringCommunity,
+    BirthdaysAnniversaries,
+    Projects,
+    LongTermGoals
+}
+
+public enum TaskImportance
+{
+    Low,
+    Medium,
+    High,
+    Critical
+}
---
+++ TaskTurner/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TaskTurner.ViewModels;
+
+public class MainWindowViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
